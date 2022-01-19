f501c490771aabfc4f6451ea7ee2ff12e6fd8426
HenryCodeT <120686@unsaac.edu.pe>
2022-01-19 13:38:10 -0500

    separate files and use inheritance

--- mascota.py
+++ mascota.py
@@ -1,8 +1,9 @@
 class Mascota:
-    def __init__(self, name , tipo , golosinas):
+    def __init__(self, name , tipo , golosinas, sonido1):
         self.name=name
         self.tipo=tipo
         self.golosinas=golosinas
+        self.sonido1=sonido1
         self.energia=0
         self.salud=0
     def dormir(self):
@@ -19,11 +20,18 @@ class Mascota:
         self.salud += 5 
         self.energia -= 5
         print(f'Salud: {self.salud}')
-    def sonido(self): 
-        if self.tipo == "dog":
-            print("guau guau guau")
-        if self.tipo == "cat":
-            print("miau miau miau")
-        if self.tipo == "chicken":
-            print("guau guau guau")
+    def sonido(self):
+        print(self.sonido1) 
+        # if self.tipo == "dog":
+        #     print("guau guau guau")
+        # if self.tipo == "cat":
+        #     print("miau miau miau")
+        # if self.tipo == "chicken":
+        #     print("guau guau guau")
 
+class TipoMascota(Mascota):
+    def __init__(self, mascota, tipo,comida_mascota,sonido1, raza,color,peso):
+        super().__init__( mascota,tipo, comida_mascota, sonido1 )
+        self.raza=raza
+        self.color=color
+        self.peso=peso
